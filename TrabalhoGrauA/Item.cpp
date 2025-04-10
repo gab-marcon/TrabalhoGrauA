edb813fd182194bfd04676ad84687c36ab0e00c1
Murilo Vasconcelos de Oliveira <86429879+muri7o@users.noreply.github.com>
2025-04-09 21:08:39 -0300

    Update Item.cpp

--- TrabalhoGrauA/Item.cpp
+++ TrabalhoGrauA/Item.cpp
@@ -10,3 +10,44 @@ Item::Item(string nome, char tipo, bool isCombate, int faBonus, int danoBonus){
 	this->faBonus = faBonus;
 	this->danoBonus = danoBonus;
 }
+
+string Item::getNome() {
+	return this->nome;
+}
+
+char Item::getTipo() {
+	return this->tipo;
+}
+
+bool Item::getIsCombate() {
+	return this->isCombate;
+}
+
+int Item::getFaBonus() {
+	return this->faBonus;
+}
+
+int Item::getDanoBonus() {
+	return this->danoBonus;
+}
+
+
+void Item::setNome(string nome) {
+	this->nome = nome;
+}
+
+void Item::setTipo(char tipo) {
+	this->tipo = tipo;
+}
+
+void Item::setIsCombate(bool isCombate) {
+	this->isCombate = isCombate;
+}
+
+void Item::setFaBonus(int faBonus) {
+	this->faBonus = faBonus;
+}
+
+void Item::setDanoBonus(int danoBonus) {
+	this->danoBonus = danoBonus;
+}
